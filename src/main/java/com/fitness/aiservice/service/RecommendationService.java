7058dc23bf0f1f6fe035fbb7b94ffbf5378501c0
Deepak <deepakkumar97967@gmail.com>
2026-05-24 20:42:00 +0530

    implemented all base packeges

---
+++ src/main/java/com/fitness/aiservice/service/RecommendationService.java
@@ -0,0 +1,12 @@
+package com.fitness.aiservice.service;
+
+import com.fitness.aiservice.model.Recommendation;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public interface RecommendationService {
+    List<Recommendation> getUserRecommendation(String userId);
+    Recommendation getActivityRecommendation(String activityId);
+}
